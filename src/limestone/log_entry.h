c7cd127c56ae39b56e0af809cf99c29e9f8d90c4
Takashi Horikawa <horikawa@nautilus-technologies.com>
2022-06-24 18:46:30 +0900

    enrich log_entry type

--- src/limestone/log_entry.h
+++ src/limestone/log_entry.h
@@ -30,14 +30,48 @@ class datastore;
 
 class log_entry {
 public:
+    enum class entry_type : std::uint8_t {
+        this_id_is_not_used,
+        normal_entry,
+        marker_begin,
+        marker_end,
+    };
+    
     log_entry() = default;
 
-// for writer
+
+    static void begin_session(boost::filesystem::ofstream& strm, epoch_id_type epoch) {
+        entry_type type = entry_type::marker_begin;
+        strm.write((char*)&type, sizeof(entry_type));
+        strm.write((char*)&epoch, sizeof(epoch_id_type));
+    }
+    static void end_session(boost::filesystem::ofstream& strm, epoch_id_type epoch) {
+        entry_type type = entry_type::marker_end;
+        strm.write((char*)&type, sizeof(entry_type));
+        strm.write((char*)&epoch, sizeof(epoch_id_type));
+    }
+
+// for writer (entry)
     void write(boost::filesystem::ofstream& strm) {
-        write(strm, storage_id_, key_, value_, write_version_);
+        switch(entry_type_) {
+        case entry_type::normal_entry:
+            write(strm, storage_id_, key_, value_, write_version_);
+            break;
+        case entry_type::marker_begin:
+            begin_session(strm, epoch_id_);
+            break;
+        case entry_type::marker_end:
+            end_session(strm, epoch_id_);
+            break;
+        case entry_type::this_id_is_not_used:
+            break;
+        }
     }
 
     static void write(boost::filesystem::ofstream& strm, storage_id_type storage_id, std::string_view key, std::string_view value, write_version_type write_version) {
+        entry_type type = entry_type::normal_entry;
+        strm.write((char*)&type, sizeof(entry_type));
+
         std::int32_t key_len = key.length();
         strm.write((char*)&key_len, sizeof(std::int32_t));
         std::int32_t value_len = value.length();
@@ -53,23 +87,36 @@ public:
 
 // for reader
     bool read(boost::filesystem::ifstream& strm) {
-        std::int32_t key_len;
-        strm.read((char*)&key_len, sizeof(std::int32_t));
+        strm.read((char*)&entry_type_, sizeof(entry_type));
         if (strm.eof()) {
             return false;
         }
-        
-        std::int32_t value_len;
-        strm.read((char*)&value_len, sizeof(std::int32_t));
 
-        strm.read((char*)&write_version_.epoch_number_, sizeof(epoch_id_type));
-        strm.read((char*)&write_version_.minor_write_version_, sizeof(std::uint64_t));
+        switch(entry_type_) {
+        case entry_type::normal_entry:
+            std::int32_t key_len;
+            strm.read((char*)&key_len, sizeof(std::int32_t));
+            std::int32_t value_len;
+            strm.read((char*)&value_len, sizeof(std::int32_t));
 
-        strm.read((char*)&storage_id_, sizeof(storage_id_type));
-        key_.resize(key_len);
-        strm.read((char*)key_.data(), key_len);
-        value_.resize(value_len);
-        strm.read((char*)value_.data(), value_len);
+            strm.read((char*)&write_version_.epoch_number_, sizeof(epoch_id_type));
+            strm.read((char*)&write_version_.minor_write_version_, sizeof(std::uint64_t));
+
+            strm.read((char*)&storage_id_, sizeof(storage_id_type));
+            key_.resize(key_len);
+            strm.read((char*)key_.data(), key_len);
+            value_.resize(value_len);
+            strm.read((char*)value_.data(), value_len);
+            break;
+
+        case entry_type::marker_begin:
+        case entry_type::marker_end:
+            strm.read((char*)&epoch_id_, sizeof(epoch_id_type));
+            break;
+
+        case entry_type::this_id_is_not_used:
+            return false;
+        }
 
         return true;
     }
@@ -86,8 +133,16 @@ public:
     void write_version(write_version_type& buf) {
         buf = write_version_;
     }
+    entry_type type() {
+        return entry_type_;
+    }
+    epoch_id_type epoch_id() {
+        return epoch_id_;
+    }
 
 private:
+    entry_type entry_type_{};
+    epoch_id_type epoch_id_{};
     storage_id_type storage_id_{};
     std::string key_{};
     std::string value_{};
---
+++ test/limestone/log/log_entry_type_test.cpp
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2022-2022 tsurugi project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <unistd.h>
+#include "test_root.h"
+
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/fstream.hpp>
+
+#include "log_entry.h"
+
+namespace limestone::testing {
+
+constexpr const char* location = "/tmp/log_entry_type_test";
+
+class log_entry_type_test : public ::testing::Test {
+protected:
+    const std::string key = "this is a key";
+    const std::string value = "this is a value";
+    const limestone::api::storage_id_type storage_id = 12345;
+    const limestone::api::write_version_type write_version = limestone::api::write_version_type(67898, 76543);
+    const limestone::api::epoch_id_type epoch_id = 56873;
+    
+    virtual void SetUp() {
+        if (system("rm -rf /tmp/log_entry_type_test") != 0) {
+            std::cerr << "cannot remove directory" << std::endl;
+        }
+        if (system("mkdir -p /tmp/log_entry_type_test") != 0) {
+            std::cerr << "cannot make directory" << std::endl;
+        }
+        file1_ = boost::filesystem::path(location) / boost::filesystem::path("file1");
+        file2_ = boost::filesystem::path(location) / boost::filesystem::path("file2");
+    }
+
+    virtual void TearDown() {
+        if (system("rm -rf /tmp/log_entry_type_test") != 0) {
+            std::cerr << "cannot remove directory" << std::endl;
+        }
+    }
+
+    limestone::api::log_entry log_entry_begin_{};
+    limestone::api::log_entry log_entry_normal_{};
+    limestone::api::log_entry log_entry_end_{};
+    boost::filesystem::path file1_{};
+    boost::filesystem::path file2_{};
+};
+
+TEST_F(log_entry_type_test, write_and_read) {
+    boost::filesystem::ofstream ostrm;
+    limestone::api::log_entry log_entry;
+
+    ostrm.open(file1_, std::ios_base::out | std::ios_base::app | std::ios_base::binary);
+    limestone::api::log_entry::begin_session(ostrm, epoch_id);
+    limestone::api::log_entry::write(ostrm, storage_id, key, value, write_version);
+    limestone::api::log_entry::end_session(ostrm, epoch_id + 1);
+    ostrm.close();
+
+    boost::filesystem::ifstream istrm;
+    istrm.open(file1_, std::ios_base::in | std::ios_base::binary);
+    EXPECT_TRUE(log_entry_begin_.read(istrm));
+    EXPECT_TRUE(log_entry_normal_.read(istrm));
+    EXPECT_TRUE(log_entry_end_.read(istrm));
+    EXPECT_FALSE(log_entry.read(istrm));
+    istrm.close();
+
+    EXPECT_EQ(log_entry_begin_.type(), limestone::api::log_entry::entry_type::marker_begin);
+    EXPECT_EQ(log_entry_begin_.epoch_id(), epoch_id);
+
+    EXPECT_EQ(log_entry_normal_.type(), limestone::api::log_entry::entry_type::normal_entry);
+    EXPECT_EQ(log_entry_normal_.storage(), storage_id);
+
+    std::string buf_key;
+    log_entry_normal_.key(buf_key);
+    EXPECT_EQ(buf_key, key);
+
+    std::string buf_value;
+    log_entry_normal_.value(buf_value);
+    EXPECT_EQ(buf_value, value);
+
+    limestone::api::write_version_type buf_version;
+    log_entry_normal_.write_version(buf_version);
+    EXPECT_TRUE(buf_version == write_version);
+
+    EXPECT_EQ(log_entry_end_.type(), limestone::api::log_entry::entry_type::marker_end);
+    EXPECT_EQ(log_entry_end_.epoch_id(), epoch_id + 1);
+}
+
+TEST_F(log_entry_type_test, write_and_read_and_write_and_read) {
+    boost::filesystem::ofstream ostrm;
+    limestone::api::log_entry log_entry;
+    
+    ostrm.open(file1_, std::ios_base::out | std::ios_base::app | std::ios_base::binary);
+    limestone::api::log_entry::begin_session(ostrm, epoch_id);
+    limestone::api::log_entry::write(ostrm, storage_id, key, value, write_version);
+    limestone::api::log_entry::end_session(ostrm, epoch_id + 1);
+    ostrm.close();
+
+    boost::filesystem::ifstream istrm;
+    istrm.open(file1_, std::ios_base::in | std::ios_base::binary);
+    boost::filesystem::ofstream ostrm2;
+    ostrm2.open(file2_, std::ios_base::out | std::ios_base::app | std::ios_base::binary);
+    while(log_entry.read(istrm)) {
+        log_entry.write(ostrm2);
+    }
+    istrm.close();
+    ostrm2.close();
+
+    boost::filesystem::ifstream istrm2;
+    istrm2.open(file2_, std::ios_base::in | std::ios_base::binary);
+    EXPECT_TRUE(log_entry_begin_.read(istrm2));
+    EXPECT_TRUE(log_entry_normal_.read(istrm2));
+    EXPECT_TRUE(log_entry_end_.read(istrm2));
+    EXPECT_FALSE(log_entry.read(istrm2));
+    istrm2.close();
+
+
+    EXPECT_EQ(log_entry_begin_.type(), limestone::api::log_entry::entry_type::marker_begin);
+    EXPECT_EQ(log_entry_begin_.epoch_id(), epoch_id);
+
+    EXPECT_EQ(log_entry_normal_.type(), limestone::api::log_entry::entry_type::normal_entry);
+    EXPECT_EQ(log_entry_normal_.storage(), storage_id);
+
+    std::string buf_key;
+    log_entry_normal_.key(buf_key);
+    EXPECT_EQ(buf_key, key);
+
+    std::string buf_value;
+    log_entry_normal_.value(buf_value);
+    EXPECT_EQ(buf_value, value);
+
+    limestone::api::write_version_type buf_version;
+    log_entry_normal_.write_version(buf_version);
+    EXPECT_TRUE(buf_version == write_version);
+
+    EXPECT_EQ(log_entry_end_.type(), limestone::api::log_entry::entry_type::marker_end);
+    EXPECT_EQ(log_entry_end_.epoch_id(), epoch_id + 1);
+}
+
+}  // namespace limestone::testing
