7bee4cb81a1474f4aae3d5ff288bc80f8b5fae5e
Nobuhiro Ban <ban@nautilus-technologies.com>
2023-12-04 21:45:45 +0900

    Add data migration tool (logdir format v0 to v1)

---
+++ dist/migration/v1/v0tov1.sh
@@ -0,0 +1,74 @@
+#! /bin/sh
+
+# converting limestone v0 dir to v1 dir
+
+USAGE="\
+usage: $0 OLD_DB_DIR DEST_DB_DIR
+	convert data in OLD_DB_DIR and store into DEST_DB_DIR.
+	DEST_DB_DIR must not contain any data.
+   or: $0 -w DB_DIR
+	convert data in DB_DIR inplace."
+
+set -e
+
+error () { echo "$*" >&2; exit 1; }
+
+check_from_dir () {
+  local dbdir="$1"
+  [ -d "$dbdir" ] || error "error: directory '$dbdir' not found."
+  [ -r "$dbdir/epoch" ] \
+    || error "error: directory '$dbdir' does not look like DB_DIR."
+  [ ! -e "$dbdir/limestone-manifest.json" ] \
+    || error "error: directory '$dbdir' does not look like v0 DB_DIR. already converted?"
+  expr '(' `stat --format=%s "$dbdir/epoch"` % 9 ')' = 0 > /dev/null \
+    || error "error: epoch file '$dbdir/epoch' is broken."
+  [ -n "`ls -A1q "$dbdir"/pwal_* 2>/dev/null | head -1`" ] \
+    || error "error: directory '$dbdir' does not look like DB_DIR. no wal files found. no need to migrate?"
+}
+
+check_to_dir () {
+  [ -w "$1" ] || error "error: directory '$1' is not writable."
+}
+
+check_dir_empty () {
+  [ -z "`ls -A1q "$1" | head -1`" ] || error "error: '$1' is not empty."
+}
+
+create_or_append_files () {
+  printf '\004\377\377\377\377\377\377\000\000' >> "$1/epoch"
+  cat > "$1/limestone-manifest.json" <<'EOD'
+{
+    "format_version": "1.0",
+    "persistent_format_version": 1
+}
+EOD
+}
+
+
+[ -n "$2" ] || error "$USAGE"
+
+echo checking...
+
+if [ "x$1" = "x-w" ]; then
+  # overwrite mode
+  check_from_dir "$2"
+  check_to_dir "$2"
+  [ -w "$2/epoch" ] || error "error: file '$2/epoch' is not writable."
+  echo converting...
+  create_or_append_files "$2"
+else
+  check_from_dir "$1"
+  if [ -d "$2" ]; then
+    check_to_dir "$2"
+    check_dir_empty "$2"
+  else
+    mkdir -p "$2"
+  fi
+  echo converting...
+  cp -p "$1"/pwal* "$2"/
+  chmod u+w "$2"/pwal_0??? 2>/dev/null || true
+  create_or_append_files "$2"
+fi
+
+echo done
+
